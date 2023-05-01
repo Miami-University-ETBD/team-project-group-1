4536b9c4422d6a069ed65d3e8e97a689202650d4
hnahk <98783276+hnahk@users.noreply.github.com>
2023-05-01 18:14:49 -0400

    update

--- java.js
+++ java.js
@@ -1,7 +1,9 @@
 let mybutton = document.getElementById("top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
